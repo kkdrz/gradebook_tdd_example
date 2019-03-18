2ee9d13ec1720a11c7d1366daa25340da46073a3
Konrad Drozd <drozdkonrad@gmail.com>
2019-03-18 23:15:05 +0100

    #6 - RED - Computing average note for a subject

--- src/test/java/pl/edu/pwr/zwo/gradebook/SubjectTest.java
+++ src/test/java/pl/edu/pwr/zwo/gradebook/SubjectTest.java
@@ -2,6 +2,7 @@ package pl.edu.pwr.zwo.gradebook;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class SubjectTest {
@@ -13,4 +14,12 @@ public class SubjectTest {
         assertNotNull(subject);
     }
 
+    @Test
+    public void shouldBeAbleToComputeAverageNote() {
+        Subject subject = new Subject("matematyka");
+        subject.addNote(4);
+        subject.addNote(5);
+        
+        assertEquals(4.5, subject.getAverage(), 0.001);
+    }
 }
