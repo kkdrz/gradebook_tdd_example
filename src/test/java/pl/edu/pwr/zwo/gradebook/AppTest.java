b484ec1af8541db06106aed7c8f571f4608aad20
Konrad Drozd <drozdkonrad@gmail.com>
2019-03-18 21:57:19 +0100

    Generate maven-quickstart archetype

---
+++ src/test/java/pl/edu/pwr/zwo/gradebook/AppTest.java
@@ -0,0 +1,20 @@
+package pl.edu.pwr.zwo.gradebook;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
