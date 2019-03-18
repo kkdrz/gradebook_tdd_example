c4bf20317e42e890ecefe7ed3a9c4aa198ef03fe
Konrad Drozd <drozdkonrad@gmail.com>
2019-03-18 22:35:08 +0100

    #3 - GREEN - Adding subject to GradeBook (single)

--- src/main/java/pl/edu/pwr/zwo/gradebook/GradeBook.java
+++ src/main/java/pl/edu/pwr/zwo/gradebook/GradeBook.java
@@ -1,5 +1,13 @@
 package pl.edu.pwr.zwo.gradebook;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class GradeBook {
 
+    private List<Subject> subjects = new LinkedList<>();
+
+    public void addSubject(Subject subject) {
+        subjects.add(subject);
+    }
 }
