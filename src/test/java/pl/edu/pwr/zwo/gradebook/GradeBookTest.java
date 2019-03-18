4ea242865f45b065c4de4b97cce94311a7c42582
Konrad Drozd <drozdkonrad@gmail.com>
2019-03-18 22:34:04 +0100

    #3 - RED - Adding subject to GradeBook (single)

--- src/test/java/pl/edu/pwr/zwo/gradebook/GradeBookTest.java
+++ src/test/java/pl/edu/pwr/zwo/gradebook/GradeBookTest.java
@@ -9,6 +9,14 @@ public class GradeBookTest {
     @Test
     public void shouldCreateGradeBook() {
         GradeBook gradeBook = new GradeBook();
+
         assertNotNull(gradeBook);
+    }
+
+    @Test
+    public void shouldAddNewSubject() {
+        GradeBook gradeBook = new GradeBook();
+
+        gradeBook.addSubject(new Subject("matematyka"));
     }
 }
